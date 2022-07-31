92d5dc32cbfee8d31761e87fe7db09af5c22938e
Denis Zolotarev <24476868+dzolotarev@users.noreply.github.com>
2022-07-31 17:14:31 +0200

    Delete 1.JavaSyntax/src/com/javarush/task/pro directory

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1315/Season.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.task.pro.task13.task1315;
-
-public enum Season {
-    WINTER,
-    SPRING,
-    SUMMER,
-    AUTUMN
-}
--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Eagle.java
+++
@@ -1,7 +0,0 @@
-package com.javarush.task.pro.task17.task1717;
-
-public class Eagle implements Flyable {
-    public int getMaxSpeed() {
-        return 180;
-    }
-}
