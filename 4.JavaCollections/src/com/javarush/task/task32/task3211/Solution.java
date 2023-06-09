7d8bc6bbaff8ede094fa0ebfedf17f022a93cc64
Denis Zolotarev <judaeus@gmail.com>
2023-06-10 00:00:21 +0200

    BufferedReader, BufferedWriter

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        messageDigest.update(byteArrayOutputStream.toByteArray());
+        byte[] digest = messageDigest.digest();
+        return byteArrayToHex(digest).equals(md5);
+    }
+
+    private static String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for(byte b: a)
+            sb.append(String.format("%02x", b));
+        return sb.toString();
+    }
+}
