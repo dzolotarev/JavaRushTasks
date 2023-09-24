6de212ca7906d3b7c869df49a1bde40ff4d9e930
Denis Zolotarev <judaeus@gmail.com>
2023-09-24 22:06:07 +0200

    Generics 2

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task35.task3512;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Generator<T> {
+    private Class<T> clazz;
+
+    public Generator(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    T newInstance() {
+        try {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
