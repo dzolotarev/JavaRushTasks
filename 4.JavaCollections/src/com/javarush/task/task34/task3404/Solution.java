8451fdcc111f9b3c5bf4a852220e6cc36291f0b1
Denis Zolotarev <judaeus@gmail.com>
2024-01-06 23:44:30 +0200

    * update tasks

--- 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -282,7 +282,7 @@ public class Solution {
         Pattern compareWithDecimalss = Pattern.compile("^-?((\\d*\\.\\d*([eE][\\+\\-]?\\d+)?)|\\d+)$");
         Matcher mmm = compareWithDecimalss.matcher(expression);
         if (mmm.matches()) {
-            Double d = new Double(expression);
+            Double d = Double.valueOf(expression);
             NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.ENGLISH);
             numberFormat.setRoundingMode(RoundingMode.HALF_EVEN);
             DecimalFormat df = (DecimalFormat) numberFormat;
