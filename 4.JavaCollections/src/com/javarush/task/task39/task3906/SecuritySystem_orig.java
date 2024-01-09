6eb2b545534e747fcd33436eee7dceda9e88d117
Denis Zolotarev <judaeus@gmail.com>
2024-01-09 14:39:17 +0200

    * CashMachine and many other

--- 4.JavaCollections/src/com/javarush/task/task39/task3906/SecuritySystem_orig.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3906/SecuritySystem_orig.java
@@ -1,19 +1,19 @@
-package com.javarush.task.task39.task3906;
-
-public class SecuritySystem_orig {
-    private boolean on = false;
-
-    public boolean isOn() {
-        return on;
-    }
-
-    void turnOff() {
-        System.out.println("Turning off the SecuritySystem!");
-        on = false;
-    }
-
-    void turnOn() {
-        System.out.println("Turning on the SecuritySystem!");
-        on = true;
-    }
-}
+//package com.javarush.task.task39.task3906;
+//
+//public class SecuritySystem_orig {
+//    private boolean on = false;
+//
+//    public boolean isOn() {
+//        return on;
+//    }
+//
+//    void turnOff() {
+//        System.out.println("Turning off the SecuritySystem!");
+//        on = false;
+//    }
+//
+//    void turnOn() {
+//        System.out.println("Turning on the SecuritySystem!");
+//        on = true;
+//    }
+//}
